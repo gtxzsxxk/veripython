f9baccfe72216f29c936bac41c6f74143458e3d8
Steven <xu-hr22@mails.tsinghua.edu.cn>
2024-11-01 23:58:43 +0800

    feat: implement token fetching buffer for lookahead in parser
    Signed-off-by: Steven <xu-hr22@mails.tsinghua.edu.cn>

--- Parser.cpp
+++ Parser.cpp
@@ -27,10 +27,37 @@ Parser::~Parser() {
     fclose(yyin);
 }
 
-std::tuple<bool, enum VeriPythonTokens> Parser::nextToken() {
-    int ret = yylex();
-    if (ret == 0) {
-        return {false, TOKEN_assign};
+std::tuple<bool, LexTokenType> Parser::lookAhead() {
+    if (tokenBuffer.empty()) {
+        auto [tokenRdy, tokenData] = nextToken();
+        if (tokenRdy) {
+            tokenBuffer.emplace(tokenData);
+            return {tokenRdy, tokenData};
+        }
+        return {tokenRdy, tokenData};
+    } else {
+        auto tokenData = tokenBuffer.front();
+        return {true, tokenData};
     }
-    return {true, VeriPythonTokens(ret)};
+}
+
+std::tuple<bool, LexTokenType> Parser::nextToken() {
+    if (tokenBuffer.empty()) {
+        int ret = yylex();
+        if (ret == 0) {
+            return {false, {TOKEN_assign, ""}};
+        }
+        std::string data{yylval.str};
+        free(yylval.str);
+        LexTokenType tokenData = {VeriPythonTokens(ret), data};
+        tokenFetched.push(tokenData);
+        if (tokenFetched.size() > TOKEN_FETCHED_SIZE) {
+            tokenFetched.pop();
+        }
+        return {true, tokenData};
+    } else {
+        auto tokenData = tokenBuffer.front();
+        tokenBuffer.pop();
+        return {true, tokenData};
+    }
 }
--- Parser.h
+++ Parser.h
@@ -8,9 +8,17 @@
 #include "Lexer.h"
 #include <string>
 #include <tuple>
+#include <queue>
+
+typedef std::pair<enum VeriPythonTokens, std::string> LexTokenType;
 
 class Parser {
     std::tuple<bool, enum VeriPythonTokens> nextToken();
+    std::queue<LexTokenType> tokenBuffer;
+
+    std::tuple<bool, LexTokenType> lookAhead();
+
+    std::tuple<bool, LexTokenType> nextToken();
 
 public:
     explicit Parser(const std::string &filename);
