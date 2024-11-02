1b54186cbb52bb2f1b660f17e42f5ebf7bae8e0c
Altocumuli <yangjh22@mails.tsinghua.edu.cn>
2024-11-03 00:00:57 +0800

    tests: feat: Include the Verilog source code for a full adder
    Signed-off-by: Altocumuli <yangjh22@mails.tsinghua.edu.cn>

---
+++ tests/verilog_srcs/full_adder.v
@@ -0,0 +1,8 @@
+module full_adder(y,carryout,a,b,carryin);
+  output y, carryout;
+  input a,b,carryin;
+
+  assign y = a ^ b ^ carryin;
+  assign carryout = ( a & b ) | ( a & carryin ) | ( b & carryin );
+
+endmodule
