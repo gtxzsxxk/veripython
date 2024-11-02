0970ced8fd45faee66532cc8cc9304fff1213b6d
Altocumuli <yangjh22@mails.tsinghua.edu.cn>
2024-11-03 00:39:51 +0800

    tests: refactor: Add a wire for the full adder to test the parser
    Signed-off-by: Altocumuli <yangjh22@mails.tsinghua.edu.cn>

--- tests/verilog_srcs/full_adder.v
+++ tests/verilog_srcs/full_adder.v
@@ -2,7 +2,12 @@ module full_adder(y,carryout,a,b,carryin);
   output y, carryout;
   input a,b,carryin;
 
-  assign y = a ^ b ^ carryin;
-  assign carryout = ( a & b ) | ( a & carryin ) | ( b & carryin );
+  wire test = a ^ b;
+  wire test2;
+
+  assign test2 = ( a & b ) | ( a & carryin ) | ( b & carryin );
+
+  assign y = test ^ carryin;
+  assign carryout = test2;
 
 endmodule
