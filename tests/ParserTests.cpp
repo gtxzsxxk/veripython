65033ac04aee61f18d0982ffb1099d9d08fe7db5
gtxzsxxk <2524395907@qq.com>
2024-11-02 20:17:28 +0800

    parser: feat: use bash to evaluate the expression and compare with our answer
    Signed-off-by: gtxzsxxk <2524395907@qq.com>

--- tests/ParserTests.cpp
+++ tests/ParserTests.cpp
@@ -11,32 +11,64 @@
 #include <ctime>
 #include <cstdio>
 
+int evaluateExpression(const std::string &expression) {
+    std::string command = "echo $(( " + expression + " ))";
+    FILE *pipe = popen(command.c_str(), "r");
+    if (!pipe) {
+        std::cerr << "Failed to run command\n";
+        return 0;
+    }
+
+    int result;
+    fscanf(pipe, "%d", &result);
+    pclose(pipe);
+    return result;
+}
+
 std::string generateComplexExpression(int depth) {
     if (depth == 0) {
-        return std::to_string(rand() % 100); // 生成0到99的随机数
+        return std::to_string((rand() % 50) + 1);
     }
 
     std::ostringstream expression;
+    remake:
+    expression.clear();
     expression << generateComplexExpression(depth - 1) << " ";
 
     char operators[] = {'+', '-', '*', '/'};
     char op = operators[rand() % 4]; // 随机选择一个操作符
-    expression << op << " " << generateComplexExpression(depth - 1);
+    std::string rhsExpr = generateComplexExpression(depth - 1);
+    if (op == '/') {
+        while (evaluateExpression(rhsExpr) == 0) {
+            rhsExpr = generateComplexExpression(depth - 1);
+        }
+    }
+    expression << op << " " << rhsExpr;
 
-    return expression.str();
+    std::string answer = expression.str();
+    if (evaluateExpression(answer) == 0) {
+        goto remake;
+    }
+
+    return answer;
 }
 
 TEST(ParserTests, ConstantExpressionParsing) {
     const std::string filename = "expressions.v";
     srand(static_cast<unsigned int>(time(nullptr)));
-    std::cout << std::filesystem::current_path() << std::endl;
-    std::ofstream outFile(filename);
-    outFile << "42 - 80 - 79 / 33 - 62 / 40 + 42 - 52" << std::endl;
-    outFile.close();
 
-    auto parser = Parser(filename);
-    auto *ast = parser.parseConstantExpr();
-    std::cout << ast->toString() << std::endl;
+    for (int i = 0; i < 100; i++) {
+        std::ofstream outFile(filename);
+        auto expressionString = generateComplexExpression((rand() % 6) + 1);
+        std::cout << "Testing: " << expressionString << std::endl;
+        int realValue = evaluateExpression(expressionString);
+        outFile << expressionString << std::endl;
+        outFile.close();
 
-    std::remove(filename.c_str());
+        auto parser = Parser(filename);
+        auto *ast = parser.parseConstantExpr();
+        int parserValue = ast->eval();
+        EXPECT_EQ(parserValue, realValue);
+        std::remove(filename.c_str());
+    }
 }
