63b4d6c2879090990224e3cd8da31712cc752399
gtxzsxxk <2524395907@qq.com>
2024-11-11 18:48:41 +0800

    seq: feat: introduce sequential logic support
    Signed-off-by: gtxzsxxk <2524395907@qq.com>

---
+++ SequentialLogics.cpp
@@ -0,0 +1,5 @@
+//
+// Created by hanyuan on 2024/11/11.
+//
+
+#include "SequentialLogics.h"
---
+++ SequentialLogics.h
@@ -0,0 +1,16 @@
+//
+// Created by hanyuan on 2024/11/11.
+//
+
+#ifndef VERIPYTHON_SEQUENTIALLOGICS_H
+#define VERIPYTHON_SEQUENTIALLOGICS_H
+#include <string>
+#include <vector>
+
+enum class TriggerEdgeType {
+    NOT_SPECIFIED,
+    POSITIVE_EDGE,
+    NEGATIVE_EDGE
+};
+
+#endif //VERIPYTHON_SEQUENTIALLOGICS_H
