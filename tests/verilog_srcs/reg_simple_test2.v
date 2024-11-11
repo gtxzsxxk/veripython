ced8a8833eb04172af3a703fcf3068df4dfd9d27
gtxzsxxk <2524395907@qq.com>
2024-11-11 23:57:42 +0800

    tests: feat: add a more complex example derived from reg test0
    Signed-off-by: gtxzsxxk <2524395907@qq.com>

---
+++ tests/verilog_srcs/reg_simple_test2.v
@@ -0,0 +1,25 @@
+module counter (
+    input wire clk,
+    input wire rst_n,
+    output [7:0] count
+);
+
+reg [7:0] counter_reg;
+reg [7:0] aux_counter_reg;
+assign count = counter_reg;
+
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+        counter_reg <= 8'd0;
+        aux_counter_reg <= 8'd0;
+    end
+    else begin
+        counter_reg <= counter_reg + 8'd1;
+        aux_counter_reg <= aux_counter_reg;
+        if (counter_reg[0] == 1'd1) begin
+            aux_counter_reg <= aux_counter_reg + 1;
+        end
+    end
+end
+
+endmodule
