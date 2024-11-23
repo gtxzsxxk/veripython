36b72a94233c9c08a952cd6feb8a8b2f097f3005
Hanyuan Zhao <hanyuan-z@qq.com>
2024-11-23 11:05:43 +0800

    emitFIRRTL: feat: add basic code template

--- EmitFIRRTL.cpp
+++ EmitFIRRTL.cpp
@@ -3,4 +3,22 @@
 //
 
 #include "EmitFIRRTL.h"
+#include "Parser.h"
+#include <circt/Dialect/FIRRTL/FIRRTLOps.h>
+#include <circt/Dialect/FIRRTL/FIRRTLTypes.h>
 #include <mlir/IR/MLIRContext.h>
+#include <mlir/IR/ImplicitLocOpBuilder.h>
+
+circt::Value EmitFIRRTL::emitFromSymbol(std::shared_ptr<CircuitSymbol> &symbol) {
+    if (symbol->getIdentifier().starts_with("__comb_" + Parser::operatorName[TOKEN_question])) {
+
+    }
+    return {};
+}
+
+void EmitFIRRTL::emit() {
+    auto hwConnections = rtlModule.getHardwareConnections();
+    for (const auto &conn: hwConnections) {
+
+    }
+}
--- EmitFIRRTL.h
+++ EmitFIRRTL.h
@@ -4,12 +4,25 @@
 
 #ifndef VERIPYTHON_EMITFIRRTL_H
 #define VERIPYTHON_EMITFIRRTL_H
+
 #include "RtlModule.h"
+#include <circt/Support/LLVM.h>
+#include <mlir/IR/Value.h>
+#include <mlir/IR/Builders.h>
 
 class EmitFIRRTL {
     const RtlModule &rtlModule;
+
+    circt::MLIRContext context{};
+
+    circt::OpBuilder builder{&context};
+
+    circt::Value emitFromSymbol(std::shared_ptr<CircuitSymbol> &symbol);
+
 public:
-    explicit EmitFIRRTL(const RtlModule &module): rtlModule(module) {}
+    explicit EmitFIRRTL(const RtlModule &module) : rtlModule(module) {}
+
+    void emit();
 };
 
 
