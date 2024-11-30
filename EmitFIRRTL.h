a1138f6b9ac5c7fff7ef34f2a4fbd8bb427c60ce
gtxzsxxk <2524395907@qq.com>
2024-11-30 21:58:56 +0800

    FIRRTL: refactor: use Implicit Location Operation Builder

--- EmitFIRRTL.h
+++ EmitFIRRTL.h
@@ -6,20 +6,27 @@
 #define VERIPYTHON_EMITFIRRTL_H
 
 #include "RtlModule.h"
+#include <unordered_map>
 #include <circt/Support/LLVM.h>
 #include <mlir/IR/Value.h>
 #include <mlir/IR/Builders.h>
+#include <mlir/IR/ImplicitLocOpBuilder.h>
 
 class EmitFIRRTL {
     const RtlModule &rtlModule;
+
+    const circt::ArrayAttr emptyArrayAttr;
 
     std::unordered_map<std::string, circt::Value> symbolTable{};
 
     circt::MLIRContext context{};
 
-    circt::OpBuilder builder{&context};
+    circt::Value
+    emitFromSymbol(const std::shared_ptr<CircuitSymbol> &symbol, const PortSlicingAST &inputSlicing = {-1, -1});
 
-    circt::Value emitFromSymbol(std::shared_ptr<CircuitSymbol> &symbol);
+    circt::ImplicitLocOpBuilder implicitLocOpBuilder{mlir::UnknownLoc::get(&context), &context};
+
+    [[nodiscard]] bool isOutputPort(std::string &identifier) const;
 
 public:
     explicit EmitFIRRTL(const RtlModule &module) : rtlModule(module) {}
