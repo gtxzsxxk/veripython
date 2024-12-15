ff6136a4e1665c07a755acef6f779a087b84244a
gtxzsxxk <2524395907@qq.com>
2024-12-15 10:46:42 +0800

    tests: fix: make the reg simple test 1 friendly with this tool

--- tests/verilog_srcs/reg_simple_test1.v
+++ tests/verilog_srcs/reg_simple_test1.v
@@ -5,25 +5,28 @@ module counter (
     output reg [3:0] comb
 );
 
+reg [7:0] count_reg;
+
 always @(*) begin
-    if (!clk) begin
-        comb <= {clk, rst_n, 2'd1};
+    if (!rst_n) begin
+        comb <= {1'd0, rst_n, 2'd1};
     end
     else begin
         if (count[0]) begin
-            comb <= {clk, rst_n, 2'd2};
+            comb <= {1'd1, rst_n, 2'd2};
         end
         else begin
-            comb <= {clk, rst_n, 2'd3};
+            comb <= {1'd0, rst_n, 2'd3};
         end
     end
+    count <= count_reg;
 end
 
 always @(posedge clk) begin
     if (!rst_n)
-        count <= 8'd0;
+        count_reg <= 8'd0;
     else
-        count <= count + 8'd1;
+        count_reg <= count_reg + 8'd1;
 end
 
 endmodule
