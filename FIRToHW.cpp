7a08580d0428bfe557d33584416782c075405003
gtxzsxxk <2524395907@qq.com>
2024-12-15 10:38:33 +0800

    HW: feat: introduce FIRRTL to HW conversion from circt passes

---
+++ FIRToHW.cpp
@@ -0,0 +1,31 @@
+//
+// Created by hanyuan on 2024/12/14.
+//
+
+#include "FIRToHW.h"
+#include "Exceptions.h"
+#include <circt/Firtool/Firtool.h>
+#include <circt/Conversion/Passes.h>
+#include <circt/Dialect/FIRRTL/FIRRTLDialect.h>
+#include <circt/Dialect/HW/HWDialect.h>
+#include <mlir/Pass/PassManager.h>
+
+void FIRToHW::convertToHW(mlir::ModuleOp &moduleOp) {
+    circt::registerLowerFIRRTLToHW();
+    context.loadDialect<circt::hw::HWDialect>();
+
+    mlir::PassManager pm(&context);
+    circt::firtool::FirtoolOptions opt{};
+    if (mlir::failed(circt::firtool::populatePreprocessTransforms(pm, opt))) {
+        throw CircuitException("Failed to run preprocess");
+    }
+    if (mlir::failed(circt::firtool::populateCHIRRTLToLowFIRRTL(pm, opt, ""))) {
+        throw CircuitException("Failed to run firrtl to low firrtl");
+    }
+    if (mlir::failed(circt::firtool::populateLowFIRRTLToHW(pm, opt))) {
+        throw CircuitException("Failed to run low firrtl to hw");
+    }
+    if (mlir::failed(pm.run(moduleOp))) {
+        throw CircuitException("Failed to run pass manager");
+    }
+}
---
+++ FIRToHW.h
@@ -0,0 +1,19 @@
+//
+// Created by hanyuan on 2024/12/14.
+//
+
+#ifndef VERIPYTHON_FIRTOHW_H
+#define VERIPYTHON_FIRTOHW_H
+
+#include <mlir/IR/BuiltinOps.h>
+
+class FIRToHW {
+    mlir::MLIRContext &context;
+public:
+    explicit
+    FIRToHW(mlir::MLIRContext &context) : context(context) {}
+
+    void convertToHW(mlir::ModuleOp &moduleOp);
+};
+
+#endif //VERIPYTHON_FIRTOHW_H
